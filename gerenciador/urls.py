8ee041403d73ecb821373a4862c157e2a0f2bd47
PietroDSouzaRangel <desouzarangelpietro344@gmail.com>
2026-06-09 22:04:18 -0300

    Atualizar o urls.py

--- gerenciador/urls.py
+++ gerenciador/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='gerenciador/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    
+
     # Rotas do CRUD
     path('', views.lista_arquivos, name='lista_arquivos'),
     path('editar/<int:arquivo_id>/', views.lista_arquivos, name='abrir_editor'),
@@ -16,7 +16,8 @@ urlpatterns = [
     path('deletar/<int:arquivo_id>/', views.deletar_arquivo, name='deletar_arquivo'),
     path('upload/', views.upload_arquivo, name='upload_arquivo'),
     path('preview/<int:arquivo_id>/', views.visualizar_html, name='visualizar_html'),
+    
+    # Rotas do Console Interativo (Bash e Python)
     path('executar-codigo/', views.executar_codigo_view, name='executar_codigo'),
-
-
+    path('executar-bash/', views.executar_bash_view, name='executar_bash'), # <-- Nova linha adicionada!
 ]
